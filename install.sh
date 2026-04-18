f8c6f9a55cbfb542431bc17ee15952c939cdefa4
Joel <joelcosta.work@gmail.com>
2026-04-18 14:04:02 +0100

    add global install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+INSTALL_DIR="/usr/local/bin"
+CLI_NAME="create-ks-app"
+REPO_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+echo "Installing $CLI_NAME to $INSTALL_DIR..."
+
+ln -sf "$REPO_DIR/src/cli.js" "$INSTALL_DIR/$CLI_NAME"
+chmod +x "$REPO_DIR/src/cli.js"
+
+echo "Done. Run: $CLI_NAME"
