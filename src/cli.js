ddabd34231c5777ccbea6ba90cdc8de57ac9c535
Joel <joelcosta.work@gmail.com>
2026-04-18 14:00:22 +0100

    add cli entry point

---
+++ src/cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import { mkdir } from 'fs/promises';
+import { join } from 'path';
+import { collectAnswers, buildContext } from './prompts.js';
+import { generate } from './generator.js';
+
+const program = new Command();
+
+program
+  .name('create-ks-app')
+  .description('Scaffold a production-ready client web application')
+  .version('1.0.0')
+  .action(async () => {
+    const answers = await collectAnswers();
+    const context = buildContext(answers);
+    const outputDir = join(process.cwd(), answers.projectName);
+    await mkdir(outputDir, { recursive: true });
+    await generate(answers, context, outputDir);
+  });
+
+program.parse();
